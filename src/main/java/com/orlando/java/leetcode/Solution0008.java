4ea9bd4ab6ff092a32ca2321ed8608697a5b90ce
Orlando Jin <orlando.jin@gmail.com>
2019-08-05 18:15:49 -0700

    Update Solution0008

--- src/main/java/com/orlando/java/leetcode/Solution0008.java
+++ src/main/java/com/orlando/java/leetcode/Solution0008.java
@@ -3,34 +3,37 @@ package com.orlando.java.leetcode;
 public class Solution0008 {
     public static int myAtoi(String str) {
         boolean spaceOk = true, numStart = false;
-        int res = 0, sign = 1;
+        int res = 0, old = 0, sign = 1;
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if (spaceOk) {
-                if (c == ' ') {
-                    continue;
-                } else {
-                    spaceOk = false;
+            if (c == ' ') {
+                if (!spaceOk) {
+                    break;
                 }
             } else {
-                if (!numStart) {
-                    if ((c == '-')) {
-                        sign = -1;
-                    }
-                    numStart = true;
-                } else {
-                    if ((c <= '0') && (c >= '9')) {
-                        res = res * 10 + c - 48;
+                spaceOk = false;
+                if ((c == '-') || (c == '+')) {
+                    if (!numStart) {
+                        sign = c == '-' ? -1 : 1;
+                        numStart = true;
                     } else {
                         break;
                     }
+                } else if ((c >= '0') && (c <= '9')) {
+                    old = res;
+                    res = res * 10 + c - 48;
+                    if (((res - c + 48) / 10) != old) {
+                        return sign > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                    }
+                } else {
+                    break;
                 }
             }
         }
-        return res;
+        return sign > 0 ? res : -res;
     }
 
     public static void main(String[] args) {
-        System.out.println(myAtoi("456"));
+        System.out.println(myAtoi("2147483648"));
     }
 }
